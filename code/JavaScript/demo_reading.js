6b261271e9460608476ae9102d07591a37d9b209
pavan <pavankumar.yalamanchili24072001@gmail.com>
2023-02-17 11:28:06 +0530

    added libraries

--- code/JavaScript/demo_reading.js
+++ code/JavaScript/demo_reading.js
@@ -13,26 +13,26 @@
 // console.log("\x1b[30m\x1b[2m%s\x1b[0m", "Hello, World!");
 
 // console.log("pad".padStart(-1));
-const term = require("terminal-kit").terminal;
-console.log(term.width);
-console.log("d".padStart(148));
+// const term = require("terminal-kit").terminal;
+// console.log(term.width);
+// console.log("d".padStart(148));
 
-var term = require("terminal-kit").terminal;
-
-function question() {
-  term("Do you like javascript? [Y|n]\n");
-
-  // Exit on y and ENTER key
-  // Ask again on n
-  term.yesOrNo({ yes: ["y", "ENTER"], no: ["n"] }, function (error, result) {
-    if (result) {
-      term.green("'Yes' detected! Good bye!\n");
-      process.exit();
-    } else {
-      term.red("'No' detected, are you sure?\n");
-      question();
-    }
-  });
-}
-
-question();
+// var term = require("terminal-kit").terminal;
+// term.windowTitle("user2");
+// while (true) {
+//   process.stdout.write("/");
+//   term.backDelete();
+//   process.stdout.write(``);
+// }
+//process.stdout.write("Waiting for authentication ");
+var twirlTimer = (function () {
+  var P = ["\\", "|", "/", "-"];
+  var x = 0;
+  return setInterval(function () {
+    process.stdout.write("\r" + "Waiting for response " + P[x++]);
+    x &= 3;
+  }, 250);
+})();
+setTimeout(() => {
+  clearTimeout(twirlTimer);
+}, 5000);
